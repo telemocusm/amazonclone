ebbbb8dbd7ba9d21ba1657d1e32ac6e098c7d3cb
telemocusm <telemocusm.dev@gmail.com>
2024-07-31 13:43:40 +0200

    Modified files

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes , Route} from 'react-router-dom'
+import Home from './component/home/Home';
+import Product from './component/product/Product';
+import Header from './component/layout/Header';
+import ProductDetails from './component/layout/ProductDetails';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header/>
+      <Routes>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/product' element={<Product/>}></Route>
+        <Route path='/product-detail/:' element={<ProductDetails/>}></Route>
+      </Routes>
+
+    </>
+  )
 }
 
 export default App;
